bd909459bdd129d5dcac161bf538ce27af1b663b
Etienne Cotin <etienne.cotin@gmail.com>
2019-05-09 17:49:15 +0200

    add user signup and login

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,11 +3,15 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\RegisterType;
+use App\Services\EmailService;
+use App\Utils\TokenModel;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
@@ -46,5 +50,64 @@ class SecurityController extends AbstractController
     public function api(): Response
     {
         return new Response(sprintf('Logged in as %s', $this->getUser()->getUsername()));
+    }
+
+    /**
+     * @Route("/login", name="app_login")
+     * @param AuthenticationUtils $authenticationUtils
+     * @return Response
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    /**
+     * @Route("/register", name="app_register")
+     * @param Request $request
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     * @param TokenModel $tokenModel
+     * @param EmailService $emailService
+     * @return Response
+     * @throws \Exception
+     */
+    public function appRegister(Request $request, UserPasswordEncoderInterface $passwordEncoder, TokenModel $tokenModel, EmailService $emailService): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegisterType::class, $user);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setPassword(
+                $passwordEncoder->encodePassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+            $user->setEnabled(true);
+            $user->setToken($tokenModel->generateToken());
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $emailService->sendRegistration($user->getEmail());
+
+            $this->addFlash(
+                'success',
+                'Formulaire bien envoyé'
+            );
+
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('security/register.html.twig', [
+                'form' => $form->createView(),
+            ]);
     }
 }
