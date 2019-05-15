e536e3ab4da37fc5744051fc704a2657081403be
Etienne Cotin <etienne.cotin@gmail.com>
2019-05-16 00:33:46 +0200

    update created at

--- src/Entity/ProjectType.php
+++ src/Entity/ProjectType.php
@@ -2,7 +2,10 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ProjectTypeRepository")
@@ -23,12 +26,29 @@ class ProjectType
     private $name;
 
     /**
-     * @var
-     * @ORM\ManyToOne(targetEntity="Project", inversedBy="type")
+     * @var ArrayCollection
+     * @ORM\OneToMany(targetEntity="Project", mappedBy="type")
      */
     private $projects;
 
+
+    /**
+     * @var \DateTime|null
+     * @Gedmo\Timestampable(on="create")
+     * @Groups({"project"})
+     * @ORM\Column(type="datetime", nullable=true)
+     */
     private $createAt;
+
+    public function __construct()
+    {
+        $this->projects = new ArrayCollection();
+    }
+
+    public function __toString()
+    {
+        return (string) $this->name;
+    }
 
     public function getId(): ?int
     {
@@ -51,5 +71,41 @@ class ProjectType
     public function getName():? string
     {
         return $this->name;
+    }
+
+    /**
+     * @return ArrayCollection
+     */
+    public function getProjects(): ArrayCollection
+    {
+        return $this->projects;
+    }
+
+    /**
+     * @param ArrayCollection $projects
+     * @return ProjectType
+     */
+    public function setProjects(ArrayCollection $projects): ProjectType
+    {
+        $this->projects = $projects;
+        return $this;
+    }
+
+    /**
+     * @return \DateTime|null
+     */
+    public function getCreateAt(): ?\DateTime
+    {
+        return $this->createAt;
+    }
+
+    /**
+     * @param \DateTime|null $createAt
+     * @return ProjectType
+     */
+    public function setCreateAt(?\DateTime $createAt): ProjectType
+    {
+        $this->createAt = $createAt;
+        return $this;
     }
 }
