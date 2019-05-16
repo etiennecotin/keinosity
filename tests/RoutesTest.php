d61326069d76854fc1bde0a6bbb133420469128d
Etienne Cotin <etienne.cotin@gmail.com>
2019-05-16 09:51:33 +0200

    add testing app routes

---
+++ tests/RoutesTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class RoutesTest extends WebTestCase
+{
+    /**
+     * @dataProvider urlProvider
+     */
+    public function testPageIsSuccessful($url)
+    {
+        $client = self::createClient([], [
+            'PHP_AUTH_USER' => 'admin@admin.com',
+            'PHP_AUTH_PW'   => 'admin',
+        ]);
+        $client->request('GET', $url);
+
+        $this->assertTrue($client->getResponse()->isSuccessful());
+    }
+
+    public function urlProvider()
+    {
+        yield ['/'];
+        yield ['/project/'];
+        yield ['/project/type/'];
+        yield ['/user/my-profil'];
+    }
+}
